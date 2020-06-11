e6cf318e12ba4a65c86fe9460eeeb2c0c9c2687a
ERERkin <erkin1399@gmail.com>
2020-06-11 20:30:53 +0600

    First commit

---
+++ src/main/java/kg/ItAcademy/BusinessPage/BusinessPageApplication.java
@@ -0,0 +1,13 @@
+package kg.ItAcademy.BusinessPage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/ItAcademy/BusinessPage/BusinessPageApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.ItAcademy.BusinessPage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
